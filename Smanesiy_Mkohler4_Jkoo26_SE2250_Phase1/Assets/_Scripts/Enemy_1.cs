7d3fbcc0f79b8469f5b6e159e7b66b768d19ce2d
Mkohler4 <Markb19n19>
2019-02-25 15:08:51 -0500

    Project, Phase 1, Step 3
    Phase 1 Complete

---
+++ Smanesiy_Mkohler4_Jkoo26_SE2250_Phase1/Assets/_Scripts/Enemy_1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Enemy_1 : Enemy
+{
+    int rndnumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (pos.x > (Camera.main.orthographicSize* Camera.main.aspect)/2)
+        {
+            rndnumber = 1;
+        }
+      
+    }
+    public override void Move()
+    {
+        if (rndnumber == 1)
+        {
+            Vector3 tempPos = pos;
+            tempPos.x -= speed * Time.deltaTime;
+            tempPos.y -= speed * Time.deltaTime;
+            pos = tempPos;
+        }
+        else
+        {
+            Vector3 tempPos = pos;
+            tempPos.x += speed * Time.deltaTime;
+            tempPos.y -= speed * Time.deltaTime;
+            pos = tempPos;
+        }
+        
+    }
+}
